d9b9cfda5e807f7e48b0c5646ec14c2b87bba93f
Janet Ziems <jsziems@gmail.com>
2021-09-30 19:24:42 -0400

    Added dialectOptions to db.js file

--- db.js
+++ db.js
@@ -2,16 +2,13 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  // NOTE: Comment out the dialectOptions to run on local host.
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
-
-// const db = new Sequelize(
-//         process.env.DB_DBNAME,
-//         process.env.DB_USER,
-//         process.env.DB_PASS,
-//         {
-//         host: process.env.DB_HOST,
-//         dialect: 'postgres'
-//       }
-//     );
 
 module.exports = db
